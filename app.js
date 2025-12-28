858752ed468b39e88b587d3cf228f2d9363b86c1
Caio Braz <caioanizer@gmail.com>
2025-12-28 20:29:40 -0300

    teste

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const produtos = [
   { id: 9, nome: "Placa de Vídeo RTX", valor: 4200.00 },
   { id: 10, nome: "Gabinete Gamer", valor: 650.00 },
   { id: 11, nome: "Teste", valor: 620.00 },
-  { id: 12, nome: "Teste_video", valor: 400.00 },
   { id: 13, nome: "Teste_video2", valor: 250.00 }
 ];
 
