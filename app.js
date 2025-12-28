7775dde4292d8f883ee9b8d22ce01be6d81450a7
Caio Braz <caioanizer@gmail.com>
2025-12-28 19:55:56 -0300

    teste video

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const produtos = [
   { id: 8, nome: "Memória RAM 16GB", valor: 380.00 },
   { id: 9, nome: "Placa de Vídeo RTX", valor: 4200.00 },
   { id: 10, nome: "Gabinete Gamer", valor: 650.00 },
-  { id: 11, nome: "Teste", valor: 620.00 }
+  { id: 11, nome: "Teste", valor: 620.00 },
+  { id: 12, nome: "Teste_video", valor: 400.00 }
 ];
 
 // Servir arquivos estáticos
